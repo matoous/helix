40844487c030e155c15c295daebf531a42e9f5d1
Matouš Dzivjak <matousdzivjak@gmail.com>
2025-11-21 10:46:13 +0100

    chore: cleanup

--- helix-term/src/application.rs
+++ helix-term/src/application.rs
@@ -1181,8 +1181,8 @@ impl Application {
                             .editor
                             .documents
                             .values()
-                            .filter(|x| x.supports_language_server(language_server))
-                            .map(|x| x.id())
+                            .filter(|doc| doc.supports_language_server(language_server))
+                            .map(|doc| doc.id())
                             .collect();
 
                         for document in documents {
